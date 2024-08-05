926dd7c5ebdea0cbd26a9cedb3e6767746cddca5
AmadeuGMeniconiDeveloper <amadevmeni@gmail.com>
2024-08-05 16:00:30 +0100

    pm2 ecosystem postgres connection

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,12 +8,13 @@ module.exports = {
       env: {
         NODE_ENV: "test",
         PORT: 3000,
-        DATABASE_NAME: "foodexplorer-test",
+        DATABASE_NAME: "test_qb1l",
         DATABASE_DIALECT: "postgres",
-        DATABASE_HOST: "localhost",
+        DATABASE_HOST:
+          "postgresql://root:c30XcKm7gvwPN6buSzNCr9SNOzuisTbd@dpg-cqoecq2j1k6c73b3rqc0-a/test_qb1l",
         DATABASE_PORT: 5432,
         DATABASE_USER: "root",
-        DATABASE_PASSWORD: "root",
+        DATABASE_PASSWORD: "c30XcKm7gvwPN6buSzNCr9SNOzuisTbd",
         JWT_SECRET: "test",
       },
     },
