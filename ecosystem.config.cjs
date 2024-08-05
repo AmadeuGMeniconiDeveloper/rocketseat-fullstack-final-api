f66de0b287b26daeb3ff70ff51ab85704dcc41a2
AmadeuGMeniconiDeveloper <amadevmeni@gmail.com>
2024-08-05 16:08:52 +0100

    pm2 ecosystem ajust

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,8 +10,7 @@ module.exports = {
         PORT: 3000,
         DATABASE_NAME: "test_qb1l",
         DATABASE_DIALECT: "postgres",
-        DATABASE_HOST:
-          "postgresql://root:c30XcKm7gvwPN6buSzNCr9SNOzuisTbd@dpg-cqoecq2j1k6c73b3rqc0-a/test_qb1l",
+        DATABASE_HOST: "dpg-cqoecq2j1k6c73b3rqc0-a",
         DATABASE_PORT: 5432,
         DATABASE_USER: "root",
         DATABASE_PASSWORD: "c30XcKm7gvwPN6buSzNCr9SNOzuisTbd",
